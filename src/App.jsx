089efe41b581e664a8c4093c5032d8b884152844
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2024-06-14 18:25:39 +0530

    square one

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import {lazy, Suspense} from 'react';
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
-import Landing from "./components/Landing"
-const Dashboard = lazy(() => import('./components/Dashboard'))
+// make a basic router
+// render landing component first , then navigate to dashboard using CSR 
+// make the components load lazily and add a fallback
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <AppBar />
-        <Routes>
-          <Route path="/" element={<Suspense fallback={'loading...'}> <Landing /> </Suspense>} />
-          <Route path="/dashboard" element={<Suspense fallback={'loading...'}> <Dashboard /> </Suspense>} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
-}
-
-const AppBar = () => {
-  const navigate = useNavigate()
-  return (
-    <>
-      <button onClick={() => {
-        navigate('/')
-      }} >Landing</button>
-      <button onClick={() => {
-        navigate('/dashboard')
-      }} >Dashboard</button>
+      hello
     </>
   )
 }
