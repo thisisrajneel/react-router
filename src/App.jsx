3f7a58467b46372d4818ee055f2279e70253e6a1
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2024-06-14 18:48:46 +0530

    basic router

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,35 @@
 // render landing component first , then navigate to dashboard using CSR 
 // make the components load lazily and add a fallback
 
+import { BrowserRouter, Route, Router, Routes, useNavigate } from "react-router-dom"
+import Landing from './components/Landing'
+import Dashboard from './components/Dashboard'
+
 function App() {
   return (
+    <BrowserRouter>
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<Landing />}>
+          
+        </Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+          
+        </Route>
+      </Routes>
+      
+    </BrowserRouter>
+  )
+}
+
+function AppBar() {
+  const navigate = useNavigate()
+  
+
+  return (
     <>
-      hello
+      <button onClick={() => navigate('/')}>Landing</button>
+      <button onClick={() => navigate('/dashboard')}>Dashboard</button>
     </>
   )
 }
